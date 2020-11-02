1caa848817757e68a0eb7a3bda64ddaaca010193
Francisco Sousa <f.sou@hotmail.com>
2020-11-02 15:20:28 +0000

    Cria PupperMaster com código do cliente e faz interface Form

---
+++ PuppetMaster/PuppetMasterGUI.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace GUIChatClient {
+    partial class PuppetMasterGUI {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbPeerList = new System.Windows.Forms.Label();
+            this.lbConv = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPeerList
+            // 
+            this.lbPeerList.AutoSize = true;
+            this.lbPeerList.Location = new System.Drawing.Point(176, 24);
+            this.lbPeerList.Name = "lbPeerList";
+            this.lbPeerList.Size = new System.Drawing.Size(64, 15);
+            this.lbPeerList.TabIndex = 7;
+            this.lbPeerList.Text = "2. Partition";
+            // 
+            // lbConv
+            // 
+            this.lbConv.AutoSize = true;
+            this.lbConv.Location = new System.Drawing.Point(12, 24);
+            this.lbConv.Name = "lbConv";
+            this.lbConv.Size = new System.Drawing.Size(103, 15);
+            this.lbConv.TabIndex = 10;
+            this.lbConv.Text = "1. Replicate Factor";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Nº replicas";
+            this.textBox2.Size = new System.Drawing.Size(64, 23);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(323, 42);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PlaceholderText = "Server ID\'s";
+            this.textBox3.Size = new System.Drawing.Size(73, 46);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(246, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PlaceholderText = "Partition ID";
+            this.textBox4.Size = new System.Drawing.Size(71, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 42);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.PlaceholderText = "Nº replicas";
+            this.textBox5.Size = new System.Drawing.Size(90, 23);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "URL";
+            this.textBox1.Size = new System.Drawing.Size(158, 23);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 105);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.PlaceholderText = "Server ID";
+            this.textBox7.Size = new System.Drawing.Size(64, 23);
+            this.textBox7.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "3. Server";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(246, 105);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.PlaceholderText = "Min delay (ms)";
+            this.textBox6.Size = new System.Drawing.Size(47, 23);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "0";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(299, 105);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.PlaceholderText = "Maz delay (ms)";
+            this.textBox8.Size = new System.Drawing.Size(47, 23);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.Text = "0";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(246, 163);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.PlaceholderText = "Script file (local)";
+            this.textBox10.Size = new System.Drawing.Size(100, 23);
+            this.textBox10.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Client";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(12, 163);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.PlaceholderText = "Username";
+            this.textBox11.Size = new System.Drawing.Size(64, 23);
+            this.textBox11.TabIndex = 11;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(82, 163);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.PlaceholderText = "URL";
+            this.textBox12.Size = new System.Drawing.Size(158, 23);
+            this.textBox12.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(352, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // PuppetMasterGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 322);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbConv);
+            this.Controls.Add(this.lbPeerList);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "PuppetMasterGUI";
+            this.Text = "Puppet Master GUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form1_Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbPeerList;
+        private System.Windows.Forms.Label lbConv;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
