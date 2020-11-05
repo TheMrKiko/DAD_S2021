c683ba16dcbf26ce45757d40590f3c6cf6089c97
Francisco Sousa <f.sou@hotmail.com>
2020-11-05 03:26:23 +0000

    Implementa algumas chamadas do Puppet Master aos nodes (incompleto)

--- PuppetMaster/PuppetMasterLogic.cs
+++ PuppetMaster/PuppetMasterLogic.cs
@@ -15,22 +15,25 @@ namespace PuppetMaster
 {
     public interface IPuppetMasterGUI
     {
-        bool AddMsgtoGUI(string s);
+        //bool AddMsgtoGUI(string s);
     }
     public class PuppetMasterLogic : IPuppetMasterGUI
     {
-        private GrpcChannel channel;
+        private GrpcChannel channel, pcschannel;
         //private PuppetMasterService.PuppetMasterServiceClient gclient;
         //private PuppetMasterService.PuppetMasterServiceClient client;
         private GServerService.GServerServiceClient gserver;
+        private PNodeService.PNodeServiceClient pnserver;
         private ProcessCreationService.ProcessCreationServiceClient pcs;
         private Server server;
         private readonly PuppetMasterGUI guiWindow;
         private string nick;
         private readonly string hostname;
+        private readonly Dictionary<string, List<string>> partitions = new Dictionary<string, List<string>>();
+
         //private AsyncUnaryCall<BcastMsgReply> lastMsgCall;
-        private Dictionary<string, GServerService.GServerServiceClient> serverMap =
-            new Dictionary<string, GServerService.GServerServiceClient>();
+        private Dictionary<string, (GServerService.GServerServiceClient, PNodeService.PNodeServiceClient)> serverMap =
+            new Dictionary<string, (GServerService.GServerServiceClient, PNodeService.PNodeServiceClient)>();
 
         public PuppetMasterLogic(PuppetMasterGUI guiWindow, string serverHostname, int serverPort)
         {
@@ -47,21 +50,21 @@ namespace PuppetMaster
             
         }
 
-        public bool AddMsgtoGUI(string s)
-        {
-            this.guiWindow.BeginInvoke(new DelAddMsg(guiWindow.AddMsgtoGUI), new object[] { s });
-            return true;
-        }
-
         public void ReplicationFactor(int r)
         {
             return;
         }
 
+        public void Partitions(int n, string id, List<string> serverids)
+        {
+            lock (this)
+                partitions.Add(id, serverids);
+        }
+
         public void Server(string id, string url, int min_delay, int max_delay)
         {
             Uri uri = new Uri(url);
-            channel = GrpcChannel.ForAddress($"http://{uri.Host}:10000");
+            pcschannel = GrpcChannel.ForAddress($"http://{uri.Host}:10000");
             pcs = new ProcessCreationService.ProcessCreationServiceClient(channel);
             CreateServerReply reply = pcs.CreateServer(
                 new CreateServerRequest { Id = id, Url = url, MinDelay = min_delay, MaxDelay = max_delay });
@@ -70,9 +73,29 @@ namespace PuppetMaster
             {
                 channel = GrpcChannel.ForAddress(url);
                 gserver = new GServerService.GServerServiceClient(channel);
+                pnserver = new PNodeService.PNodeServiceClient(channel);
+
+                RegisterPartitionRequest req;
+                foreach (string p in partitions.Keys)
+                {
+                    req = new RegisterPartitionRequest { PartitionId = p };
+                    req.ServerIds.Add(partitions[p]);
+                    pnserver.RegisterPartition(req);
+                }
+
+                /*RegisterServerRequest reqs;
+                foreach (string s in serverMap.Keys)
+                {
+                    foreach (string s1 in serverMap[s].ke)
+                    {
+                        reqs = new RegisterServerRequest { Id=s, Url=serverMap[s] };
+                    req.ServerIds.Add(partitions[p]);
+                    pnserver.RegisterServer(reqs);
+                }*/
 
                 lock (this)
-                    serverMap.Add(url, gserver);
+                    serverMap.Add(url, (gserver, pnserver));
+
             }
             //Console.WriteLine($"Registered client {request.Nick} with URL {request.Url}");
 
@@ -83,7 +106,13 @@ namespace PuppetMaster
 
         }
 
-        /*public List<string> Register(string nick, string port)
+        /*public bool AddMsgtoGUI(string s)
+        {
+            this.guiWindow.BeginInvoke(new DelAddMsg(guiWindow.AddMsgtoGUI), new object[] { s });
+            return true;
+        }
+
+        public List<string> Register(string nick, string port)
         {
             this.nick = nick;
             // setup the client service
